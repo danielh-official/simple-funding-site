f87a45154ab9d17c65553b7b635252bd95f1c54f
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2025-09-29 16:16:32 -0400

    Fix dashboard route definition and organize dashboard sub-routes

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,23 @@ Route::get('/pricing', function () {
 })->name('pricing');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
+    Route::get('/dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+
+    Route::prefix('dashboard')->name('dashboard.')->group(function () {
+        Route::get('/create', function () {
+            return Inertia::render('dashboard/create');
+        })->name('create');
+
+        Route::get('/edit/{fundingPage}', function () {
+            return Inertia::render('dashboard/edit');
+        })->name('edit');
+
+        Route::get('/detail/{fundingPage}', function () {
+            return Inertia::render('dashboard/detail');
+        })->name('detail');
+    });
 });
 
 require __DIR__.'/settings.php';
